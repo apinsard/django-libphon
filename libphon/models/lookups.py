3271a12efb0d4c7b16659ffa19501546ae8a5109
Antoine Pinsard <antoine.pinsard@gmail.com>
2017-03-20 15:45:38 +0000

    Elegant rewrite

--- libphon/models/lookups.py
+++ libphon/models/lookups.py
@@ -12,8 +12,8 @@ class PhoneMatchLookup(Regex):
 
     def get_db_prep_lookup(self, value, connection):
         value = re.sub(r'[^0-9]', '', value)
-        m = re.match(r'(33|0+)([1-9][0-9]*)?', value)
-        if len(value) >= 6 and m and and m.group(2):
+        m = re.match(r'(33|0+)([1-9][0-9]{4,})', value)
+        if m and and m.group(2):
             value = r'^(\+?(33|590)|0+)[^0-9]*{}[^0-9]*'.format(
                 r'[^0-9]*'.join(m.group(2)))
         else:
